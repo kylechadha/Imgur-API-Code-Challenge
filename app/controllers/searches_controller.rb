82ada65e92018fc6a42a9a009e1a9f6af578e10b
Kyle Chadha <kyle.chadha@gmail.com>
2014-05-04 23:38:03 -0400

    Add error handling for invalid Twitter handles

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,12 +2,16 @@ class SearchesController < ApplicationController
   
   def index
     if params[:search] and params[:search] != ""
-      # Retrieve a list of the 100 most recent tweets by the specified user and run them through the extract_hashtags method defined on the Search model
-      binding.pry
-      @results = Search.extract_hashtags(application.user_timeline(params[:search], :count => 100))
+      # Error handling in case the user enters an invalid Twitter handle
+      begin
+        # Retrieve a list of the 100 most recent tweets by the specified user and run them through the extract_hashtags method defined on the Search model
+        @results = Search.extract_hashtags(application.user_timeline(params[:search], :count => 100))
 
-      # Search Imgur's API for pictures that match the hashtags retrieved above
-      @images = Search.imgur_search(@results)
+        # Search Imgur's API for pictures that match the hashtags retrieved above
+        @images = Search.imgur_search(@results)
+      rescue
+        @error = "Sorry! That Twitter handle does not exist."
+      end
     end
   end
 
