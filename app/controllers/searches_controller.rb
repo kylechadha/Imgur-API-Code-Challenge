33bebc2b8b1631881a446e133ed7b2e7b7dacf07
Kyle Chadha <kyle.chadha@gmail.com>
2014-05-04 23:24:00 -0400

    Style search bar

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,16 +3,11 @@ class SearchesController < ApplicationController
   def index
     if params[:search] and params[:search] != ""
       # Retrieve a list of the 100 most recent tweets by the specified user and run them through the extract_hashtags method defined on the Search model
+      binding.pry
       @results = Search.extract_hashtags(application.user_timeline(params[:search], :count => 100))
 
       # Search Imgur's API for pictures that match the hashtags retrieved above
       @images = Search.imgur_search(@results)
-    end
-
-    # Respond to AJAX javascript calls
-    respond_to do |format|
-      format.html
-      format.js
     end
   end
 
